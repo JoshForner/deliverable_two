25a8dd76e08b17583b8630014fca18f4af20862c
josh <fornerjos@gmail.com>
2020-03-04 19:09:10 -0500

    First Commit

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your message? ");
+            string input = Console.ReadLine();
+            string input1 = input.ToUpper();
+            int total = 0;
+
+            Console.Write("Your encoded message is ");
+            for (int i = 0; i < input1.Length; i++)
+            {
+                char c = input1[i];
+                int message = c - 64;
+                Console.Write(message + "-");
+            }
+            Console.WriteLine();
+            Console.Write("Message checksum is ");
+            for (int i = 0; i < input1.Length; i++)
+            {
+                
+                char c = input1[i];
+                total += c;
+                int checksum = total;
+                Console.Write(checksum);
+            }
+        }
+    }
+}
